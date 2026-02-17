afc2200d6e9995fe3d23fdb4ac66c0c913c59cc3
Younganswer <dudtj1999@gmail.com>
2026-02-17 18:46:32 +0900

    fix(backend): import outbox module in cron jobs module

--- src/service/backend/src/cron-jobs/cron-jobs.module.ts
+++ src/service/backend/src/cron-jobs/cron-jobs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { OutboxDispatchJob } from '@/cron-jobs/jobs/outbox-dispatch.job';
+import { OutboxModule } from '@/modules/outbox/outbox.module';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [CqrsModule, OutboxModule],
   providers: [OutboxDispatchJob],
 })
 export class CronJobsModule {}
