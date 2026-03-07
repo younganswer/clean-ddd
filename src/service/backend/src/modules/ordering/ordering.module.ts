f0cdfc7f72891112160692fdb6e7ae278c398cab
Younganswer <dudtj1999@gmail.com>
2026-03-07 22:35:08 +0900

    refactor(ordering): stop exporting write repository symbol

--- src/service/backend/src/modules/ordering/ordering.module.ts
+++ src/service/backend/src/modules/ordering/ordering.module.ts
@@ -8,7 +8,7 @@ import { QueryHandlers } from '@/modules/ordering/application/queries';
 import { MarkOrderPaidOnPaymentWebhookSucceededHandler } from '@/modules/ordering/application/events/handlers/mark-order-paid-on-payment-webhook-succeeded.handler';
 import { OrdersController } from '@/modules/ordering/presentation/orders.controller';
 import { OrderReaderProvider } from '@/modules/ordering/infrastructure/readers/order.reader';
-import { IOrderReaderSymbol } from '@/modules/ordering/application/readers/i.order.reader';
+import { IOrderReaderSymbol } from '@/shared/ordering/readers/i.order.reader';
 
 @Module({
 	imports: [CqrsModule],
@@ -25,6 +25,6 @@ import { IOrderReaderSymbol } from '@/modules/ordering/application/readers/i.ord
 		...QueryHandlers,
 		MarkOrderPaidOnPaymentWebhookSucceededHandler,
 	],
-	exports: [IOrderRepositorySymbol, IOrderReaderSymbol],
+	exports: [IOrderReaderSymbol],
 })
 export class OrderingModule {}
